6145a1e8e1811cdf87cf91e2dccfeb8ebe6486ad
Gpocas <gpocas01@gmail.com>
2026-05-22 08:40:44 -0300

    Capture screenshots only for errors and exceptions

--- src/jaylog/formatters.py
+++ src/jaylog/formatters.py
@@ -93,7 +93,7 @@ def build_log_entry_dict(record: logging.LogRecord) -> dict:
         "hostname": _HOSTNAME,
         "ipv4": _HOST_IP,
         "service_path": record.pathname,
-        "log_img": _capture_screenshot(),
+        "log_img": _capture_screenshot() if (record.levelno >= logging.ERROR or is_exception) else None,
     }
 
 
